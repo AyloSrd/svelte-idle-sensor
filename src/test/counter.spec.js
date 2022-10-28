3758d7bf181a46e55fbecf2379e516796a44eec6
AyloSrd <aylo.srd@gmail.com>
2022-10-28 14:51:30 +0200

    exported test in own repository

--- src/test/counter.spec.js
+++ src/test/counter.spec.js
@@ -4,11 +4,11 @@
 
 import '@testing-library/jest-dom';
 import { render } from "@testing-library/svelte";
-import Counter from "./Counter.svelte";
+import Counter from "../lib/Counter.svelte";
 
 describe("counter component", () => {
   test("should confirm there are no images in the counter ", () => {
     const { container } = render(Counter);
-    expect(container).toContainHTML("");
+    expect(container).toContainHTML("1");
   });
 });
